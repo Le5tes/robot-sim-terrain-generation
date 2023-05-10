88d63a91b43c5344c7c0243d5c4f25e8afa67d71
Tim Williamson <tim.williamson@kaluza.com>
2023-05-10 20:56:51 +0100

    add basic terrain generation
    generates an stl file and sdf file with a simple flat plane with normally distributed noise on z axis

--- terrain_generator.py
+++ terrain_generator.py
@@ -1,13 +1,66 @@
-from to_sdf import to_sdf, to_obj
+from to_sdf import to_sdf, to_obj, build_sdf_file
+from stl import mesh, Mode
+import numpy as np
 
+
+size = 10
+
+heightmap = np.random.normal(size = (size,size))
+
+plane_vertices = np.array([
+    [x,y, heightmap[x,y]]
+    for x in range(size)
+    for y in range(size)
+])
+
+plane_faces = np.array([
+    [x, x+1, x+ size] 
+    for x in range(size * (size - 1))
+    if x % size != size - 1
+] + [
+    [x+1, x+size, x+ size + 1] 
+    for x in range(size * (size - 1))
+    if x % size != size - 1
+])
+
+plane = mesh.Mesh(np.zeros(plane_faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(plane_faces):
+    for j in range(3):
+        plane.vectors[i][j] = plane_vertices[f[j],:]
+
+plane.save('plane/plane.stl')
 
 sdf = {
     '@version': '1.5',
     'model': [
-        {'@name': 'bob'}
+        {
+            '@name': 'plane', 
+            'static': True,
+            'link': {
+                '@name':'plane_link',
+                'visual': {
+                    '@name': 'plane_visual',
+                    'pose': '0 0 0 0 0 0',
+                    'geometry': {
+                        'mesh': {
+                            'uri': 'plane.stl',
+                            'scale': '1 1 1'
+                        }
+                    }
+                },
+                    'collision': {
+                    '@name': 'plane_col',
+                    'pose': '0 0 0 0 0 0',
+                    'geometry': {
+                        'mesh': {
+                            'uri': 'plane.stl',
+                            'scale': '1 1 1'
+                        }
+                    }
+                }
+            }
+        }
     ]
 }
 
-print(to_sdf(sdf))
-
-# print(to_obj('./testing/test.xml'))
+build_sdf_file(sdf, 'plane/plane.sdf')
--- to_sdf.py
+++ to_sdf.py
@@ -2,9 +2,12 @@ import xmlschema
 
 schema = xmlschema.XMLSchema('schemas/root.xsd')
 
-def to_sdf(object):
-    sdf = schema.encode(object)
+def to_sdf(obj):
+    sdf = schema.encode(obj)
     return sdf
 
 def to_obj(sdf):
     return schema.decode(sdf)
+
+def build_sdf_file(obj, filename):
+    xmlschema.XmlDocument(to_sdf(obj), schema).write(filename)
