5032cefdc3031ae921a8f8d0bed8695b4d2c7538
Tim Williamson <tim.williamson@kaluza.com>
2023-05-20 22:28:02 +0100

    setup syntax error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.1',
     author='Tim Williamson',
     author_email='timwilliamson1337@gmail.com',
-    description='Create terrain for training robots - gazebo'
+    description='Create terrain for training robots - gazebo',
     packages=['training_ground'], 
     url='git@gitlab.com:ai-masters/final-project/terrain-generation',
     install_requires=['stl', 'xml_schema']
