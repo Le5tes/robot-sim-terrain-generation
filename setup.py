cc1b951153ffdb88d57e0de1b62488b8fe6d77cb
Tim Williamson <tim.williamson@kaluza.com>
2023-05-21 09:34:18 +0100

    add include_package_data

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='gazebo-training-ground-generator',
-    version='0.0.2',
+    version='0.0.3',
     author='Tim Williamson',
     author_email='timwilliamson1337@gmail.com',
     description='Create terrain for training robots - gazebo',
-    packages=['training_ground'], 
+    packages=['training_ground'],
+    include_package_data=True,
     url='git@gitlab.com:ai-masters/final-project/terrain-generation',
     install_requires=['stl', 'xmlschema']
 )
