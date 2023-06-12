61f767a603ac1a6d15f4f88c26962e00c34401d1
Tim Williamson <tim.williamson@kaluza.com>
2023-06-12 11:01:11 +0100

    move individual terrain generators to own file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gazebo-training-ground-generator',
-    version='0.0.12',
+    version='0.0.13',
     author='Tim Williamson',
     author_email='timwilliamson1337@gmail.com',
     description='Create terrain for training robots - gazebo',
--- training_ground/terrain_generator.py
+++ training_ground/terrain_generator.py
@@ -1,4 +1,5 @@
 from training_ground.to_sdf import build_sdf_file
+from training_ground.terrain_generators import jagged_terrain, boxes_terrain
 from stl import mesh, Mode
 import os
 import numpy as np
@@ -7,7 +8,6 @@ terrain_fns = {
     'jagged': jagged_terrain,
     'boxes': boxes_terrain
 }
-
 
 def build_terrain(base_path, t_type, size, intensity):
     path = base_path + '/plane'
@@ -85,71 +85,3 @@ def build_terrain(base_path, t_type, size, intensity):
     build_sdf_file(sdf, sdf_path)
 
     return sdf_path
-
-def jagged_terrain(size, intensity):
-    jaggedness = intensity
-
-    heightmap = np.random.normal(size = (size,size), scale = jaggedness)
-
-    plane_vertices = np.array([
-        [x,y, heightmap[x,y]]
-        for x in range(size)
-        for y in range(size)
-    ])
-
-    plane_faces = np.array([
-        [x, x+size, x+1] 
-        for x in range(size * (size - 1))
-        if x % size != size - 1
-    ] + [
-        [x+1, x+size, x+size+1] 
-        for x in range(size * (size - 1))
-        if x % size != size - 1
-    ])
-
-    return plane_vertices, plane_faces
-
-def boxes_terrain(size, intensity):
-    heightmap = np.random.normal(size = (size,size), scale = intensity)
-
-    # maintain a 2d structure of boxes for readability
-    boxes = [
-        [
-            [
-                [x,y, heightmap[x,y]],
-                [x+1,y, heightmap[x,y]],
-                [x,y+1, heightmap[x,y]],
-                [x+1,y+1, heightmap[x,y]]
-            ]
-            for x in range(size)
-        ]
-        for y in range(size)
-    ]
-
-    plane_vertices = np.array([vertex for row in boxes for box in row for vertex in box]) # just flatten boxes to get the vertices
-
-    plane_faces = np.array(
-        face 
-        for item in(
-            # box tops
-            [
-                [
-                    [box[0], box[1], box[2]], 
-                    [box[1], box[2], box[3]]
-                ] for row in boxes for box in row
-            ] + 
-            # box sides (just 2 sides)
-            [
-                [
-                    [boxes[y][x][1], boxes[y][x][3], boxes[y][x+1][2]],
-                    [boxes[y][x][1], boxes[y][x+1][0], boxes[y][x+1][2]],
-                    [boxes[y][x][2], boxes[y][x][3], boxes[y+1][x][0]],
-                    [boxes[y][x][3], boxes[y+1][x][0], boxes[y+1][x][1]],
-                ] for x in range(size - 1) for y in range(size - 1)
-            ]
-        ) 
-        for face in item
-        )
-
-    return plane_vertices, plane_faces
-
