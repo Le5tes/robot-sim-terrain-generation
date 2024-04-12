283d1cb77ea61f3efdcfd8fd18221525a672d5f5
Tim Williamson <tim.williamson@kaluza.com>
2024-04-12 13:44:20 +0100

    hghj

--- training_ground/terrain_generators_heightmaps.py
+++ training_ground/terrain_generators_heightmaps.py
@@ -46,7 +46,7 @@ def potholes_terrain(size, intensity, start, goal, scale = 1):
 
 
     def holes_permutation(x, y):
-        return - (4 * ((x,y) in holes and (x,y) not in protected)) + (x,y) in protected
+        return - (4 * ((x,y) in holes and (x,y) not in protected)) + ((x,y) in protected)
 
     return jagged_terrain(size, intensity/2, start, goal, permutation = holes_permutation)
 
@@ -74,7 +74,7 @@ def pillars_terrain(size, intensity, start, goal, scale = 1 ):
     protected = get_protected(start, goal)
 
     def pillars_permutation(x,y):
-        return 10 * ((x,y) in pillars and (x,y) not in protected) + (x,y) in protected
+        return 10 * ((x,y) in pillars and (x,y) not in protected) + ((x,y) in protected)
 
     return jagged_terrain(size, intensity/2, start, goal, permutation = pillars_permutation)
 
